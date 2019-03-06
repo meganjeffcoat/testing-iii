ad0926f8b8388a75753197da0265a0761271dc3e
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-06 15:06:00 -0600

    ran test for defaults to unlocked and opened...passes

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('controls', () => {
+    it('render buttons to toggle', () => {
+        const { getByText } = render(<Controls closed={false} unlocked={false} />);
+        const lockBtn = getByText(/lock gate/i);
+        const closeBtn = getByText(/close gate/i);
+
+        expect(lockBtn).toHaveTextContent(/lock gate/i);
+
+        expect(closeBtn).toHaveTextContent(/close gate/i);
+    });
+});
